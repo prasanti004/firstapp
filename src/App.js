21cc848b4d28684d28fea6f08b878493a83e6dd7
prasanti004 <prasanthi.valle@gmail.com>
2021-10-17 20:49:27 +0530

    first checkin

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+<>
+    
+    <table cellspacing="2" >
+       <tr>
+          <td>First Name</td>
+          <td><input type="text"></input></td>
+         
+       </tr>
+       <tr>
+          <td>Last Name</td>
+          <td><input type="text"></input></td>          
+       </tr>       
+    </table>
+   
+    </>
+
+  );
+}
+
+export default App;
